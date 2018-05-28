85bbe6786fffba9b9897da3fb2cd7de9ab805237
pei ningyuan <pz763895@dal.ca>
2018-05-28 15:42:59 -0300

    test6 on travis

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -1,6 +1,6 @@
 describe('test popup', function () {
     it('popup shoulde be true', function () {
-        cy.visit('cypress_project/index.html')
+        cy.visit('/test')
         cy.get('button[popupbutton="popup-button"]').click()
 
         cy.get('#myModal').should('have.class', 'in')
